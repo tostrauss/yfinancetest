1e3152c14d44844c80d3b40aea75cd9d35d97767
PUTrades005 <jaxonholdenn40@gmail.com>
2025-02-15 23:19:27 -0500

    Update Setup Shell Script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Creating virtual environment..."
 python3 -m venv venv
-source venv/bin/activate
+source venv/Scripts/activate
 pip install -r requirements.txt
 echo "Launching Streamlit..."
-streamlit run streamlit_app.py
+streamlit run app.py
